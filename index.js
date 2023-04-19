f54927ccda69e2c2f41c41e5ef28453fa05654c3
francis <jerylfranz460@gmail.com>
2023-04-20 03:22:31 +0400

    initialized project and add exisiting missions in the game

---
+++ index.js
@@ -0,0 +1,15 @@
+import boxen from "boxen";
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+// api endpoint
+// api endpoint https://api.warframestat.us/pc/{missiontype}/queries
+//
+//
+//
+//
+//
+//
+async function callWarframeApic(type,queries){
+    const endpoint =`https://api.warframestat.us/pc/${type}/${queries}`
+}
