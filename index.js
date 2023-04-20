d045dacdf8ab22ddab720ea78ffcbe4a73c834c9
francis <jerylfranz460@gmail.com>
2023-04-20 04:19:06 +0400

    ref: able to pass in and return json data

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import chalk from "chalk";
 import inquirer from "inquirer";
 import { readFile } from "fs/promises";
 import { EventEmitter } from "events";
-
 const eventEmitter = new EventEmitter();
+const missionsJson = JSON.parse(
+  await readFile(new URL("./missions.json", import.meta.url))
+);
 
 eventEmitter.on("wf-init", (bool) => {
   console.clear();
@@ -17,10 +19,7 @@ eventEmitter.emit("wf-init", true);
 // api endpoint
 // api endpoint https://api.warframestat.us/pc/{missiontype}/queries
 
-let userInput;
-const missionsJson = JSON.parse(
-  await readFile(new URL("./missions.json", import.meta.url))
-);
+eventEmitter.on("userInput", callWarframeApi);
 
 async function getUserInputs() {
   const input = inquirer.prompt({
@@ -29,12 +28,16 @@ async function getUserInputs() {
     message: `Please Pick a Mission type to get it's current status`,
     choices: missionsJson,
   });
-  userInput = await input.input;
+  const userInput = await input;
   console.log(userInput);
-  return;
+  eventEmitter.emit("userInput", userInput);
+}
+
+async function callWarframeApi(type, queries = { language: "en" }) {
+  console.log(type);
+  const endPoint = await fetch(`https://api.warframestat.us/pc/${type.input}`);
+  const jsonData = await endPoint.json();
+  console.log(jsonData);
+  return jsonData;
 }
 getUserInputs();
-
-async function callWarframeApic(type, queries) {
-  const endpoint = `https://api.warframestat.us/pc/${type}/${queries}`;
-}
