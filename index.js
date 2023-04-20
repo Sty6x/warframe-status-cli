9d56a7bfa06e595e2c32fc2b9cc52b8ee03a887e
francis <jerylfranz460@gmail.com>
2023-04-20 05:23:45 +0400

    ref: able to fetch and display better text than camel case

--- index.js
+++ index.js
@@ -34,20 +34,21 @@ async function getUserInputs() {
   const types = await input;
   const userInput = missionsJson.filter((mission) => mission[types.input])[0];
   console.log(userInput);
-  // eventEmitter.emit("userInput", userInput);
+  eventEmitter.emit("userInput", userInput);
 }
 
 async function callWarframeApi(type, queries = { language: "en" }) {
+  const missionType = Object.keys(type)[0];
   const spinner = createSpinner(
-    `Fetching Current ${type.input} Status...`
+    `Fetching Current ${missionType} Status...`
   ).start();
   try {
     const endPoint = await fetch(
-      `https://api.warframestat.us/pc/${type.input}`
+      `https://api.warframestat.us/pc/${type[missionType]}?language=${queries.language}`
     );
     const jsonData = await endPoint.json();
     console.log(jsonData);
-    spinner.success({ text: `Here's the current status for ${type.input}` });
+    spinner.success({ text: `Here's the current status for ${missionType}` });
     return jsonData;
   } catch (err) {
     spinner.error({ text: `Unable to fetch curretn warframe status` });
