2e788887a1affa9a68657ae55bfefe12f47ce3ac
francis <jerylfranz460@gmail.com>
2023-04-20 05:15:49 +0400

    ref: displaying keys of each mission type instead of camelCases

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import chalk from "chalk";
 import inquirer from "inquirer";
 import { readFile } from "fs/promises";
 import { EventEmitter } from "events";
+import { createSpinner } from "nanospinner";
 const eventEmitter = new EventEmitter();
 const missionsJson = JSON.parse(
   await readFile(new URL("./missions.json", import.meta.url))
@@ -13,31 +14,43 @@ eventEmitter.on("wf-init", (bool) => {
   console.clear();
   if (bool) {
     console.log("Welcome to Warframe-Cli-Status");
+    getUserInputs();
   }
 });
-eventEmitter.emit("wf-init", true);
-// api endpoint
-// api endpoint https://api.warframestat.us/pc/{missiontype}/queries
 
+eventEmitter.emit("wf-init", true);
 eventEmitter.on("userInput", callWarframeApi);
 
 async function getUserInputs() {
+  const missionTypes = missionsJson.map((mission) => {
+    return Object.keys(mission)[0];
+  });
   const input = inquirer.prompt({
     name: "input",
     type: "list",
     message: `Please Pick a Mission type to get it's current status`,
-    choices: missionsJson,
+    choices: missionTypes,
   });
-  const userInput = await input;
+  const types = await input;
+  const userInput = missionsJson.filter((mission) => mission[types.input])[0];
   console.log(userInput);
-  eventEmitter.emit("userInput", userInput);
+  // eventEmitter.emit("userInput", userInput);
 }
 
 async function callWarframeApi(type, queries = { language: "en" }) {
-  console.log(type);
-  const endPoint = await fetch(`https://api.warframestat.us/pc/${type.input}`);
-  const jsonData = await endPoint.json();
-  console.log(jsonData);
-  return jsonData;
+  const spinner = createSpinner(
+    `Fetching Current ${type.input} Status...`
+  ).start();
+  try {
+    const endPoint = await fetch(
+      `https://api.warframestat.us/pc/${type.input}`
+    );
+    const jsonData = await endPoint.json();
+    console.log(jsonData);
+    spinner.success({ text: `Here's the current status for ${type.input}` });
+    return jsonData;
+  } catch (err) {
+    spinner.error({ text: `Unable to fetch curretn warframe status` });
+    throw err;
+  }
 }
-getUserInputs();
